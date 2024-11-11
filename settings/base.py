99396a0141ffd176ff2eda33707c2ef5ded334c9
ronnzw <rmaravanyika@gmail.com>
2024-11-11 12:27:51 +0200

    set default email

--- settings/base.py
+++ settings/base.py
@@ -192,3 +192,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_FROM_EMAIL = 'do-not-reply@4mayclothing.com'
